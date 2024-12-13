8f377c3ed4c37c4e7eeba2a59b7fc75e569c7fe2
V0rt3x667 <al-g0l@outlook.com>
2024-12-13 01:17:58 +0000

    Fix mem

--- scriptmodules/system.sh
+++ scriptmodules/system.sh
@@ -33,17 +33,17 @@ function test_chroot() {
 }
 
 function conf_memory_vars() {
-    __memory_total_kb=$(awk '/^MemTotal:/{print ${2}}' /proc/meminfo)
+    __memory_total_kb=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
     __memory_total=$(( __memory_total_kb / 1024 ))
     if grep -q "^MemAvailable:" "/proc/meminfo"; then
-        __memory_avail_kb=$(awk '/^MemAvailable:/{print ${2}}' /proc/meminfo)
+        __memory_avail_kb=$(awk '/^MemAvailable:/{print $2}' /proc/meminfo)
     else
         local mem_free
         local mem_cached
         local mem_buffers
-        mem_free=$(awk '/^MemFree:/{print ${2}}' /proc/meminfo)
-        mem_cached=$(awk '/^Cached:/{print ${2}}' /proc/meminfo)
-        mem_buffers=$(awk '/^Buffers:/{print ${2}}' /proc/meminfo)
+        mem_free=$(awk '/^MemFree:/{print $2}' /proc/meminfo)
+        mem_cached=$(awk '/^Cached:/{print $2}' /proc/meminfo)
+        mem_buffers=$(awk '/^Buffers:/{print $2}' /proc/meminfo)
         __memory_avail_kb=$((mem_free + mem_cached + mem_buffers))
     fi
     __memory_avail=$(( __memory_avail_kb / 1024 ))
